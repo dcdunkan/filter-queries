9711a4960c058b443b7e070e05583a8a55ebcaa3
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2023-02-28 17:39:20 +0530

    improve design and docs

--- docs.ts
+++ docs.ts
@@ -31,26 +31,31 @@ export function generate() {
     // Yes, no formatting, its already a mess, and with formatting, its really messy.
     // deno-fmt-ignore
     if (l1[0] && !l2[0] && !l3) { // message
-    info = `\
+    
+      info = `\
 Filter query for listening for ${l1text} update${s(l1)}. \
-It's properties can be accessed from:
+${plural(l1, "It's")} properties can be accessed from:
 <ul>${l1.map((k) => li(code(`ctx.update.${k}`))).join("\n")}</ul>
 or from their corresponding shortcuts:
 <ul>${l1.map((k) => li(code(`ctx.${CONTEXT_SHORTCUTS[k]}`))).join("\n")}</ul>`
-  } else if (l1[0] && l2[0] && !l3) { // message:text
-    info = `\
-Filter query for listening to ${l1text} updates containing the ${l2text} field${s(l2)}. \
+  
+    } else if (l1[0] && l2[0] && !l3) { // message:text
+    
+      info = `\
+Filter query for listening to ${l1text} update${(l1)} containing the ${l2text} field${s(l2)}. \
 The field or its properties can be accessed like:
 <ul>${l1.map((k1) => l2.map((k2) => li(code(`ctx.update.${k1}.${k2}`))).join("\n")).join("\n")}</ul>
 
-or from the shortcuts of update keys like the following:
+or from the shortcuts of update key${s(l1)} like the following:
 <ul>${l1.map((k1) => l2.map((k2) => li(code(`ctx.${CONTEXT_SHORTCUTS[k1]}.${k2}`))).join("\n")).join("\n")}</ul>`;
-  } else if (l1[0] && l2[0] && l3) { // message:entities:pre
-    info = `\
+  
+    } else if (l1[0] && l2[0] && l3) { // message:entities:pre
+    
+      info = `\
 ${
   l2text.includes("entities")
-    ? `Filter query for listening to ${code(l3)} entity, making sure that at least one entity of ${code(l3)} type exists in the ${l2text} of ${l1text} updates`
-    : `Filter query for listening to ${l1text} updates containing ${l2text} properties with the field ${code(l3)}` 
+    ? `Filter query for ${code(l3)} entity, making sure that at least one entity of ${code(l3)} type exists in the ${l2text} field${s(l2)} of ${l1text} update${s(l1)}`
+    : `Filter query for listening to ${l1text} update${s(l1)} containing ${l2text} properties with the field ${code(l3)}`
 }. These can be accessed like:
 <ul>${l1.map((k1) => l2.map((k2) => li(code(`ctx.update.${k1}.${k2}${l2text.includes("entities") ? "" : `.${l3}`}`))).join("\n")).join("\n")}</ul>
 
@@ -58,13 +63,16 @@ Or use the shortcuts for update type:
 <ul>${l1.map((k1) => l2.map((k2) => li(code(`ctx.${CONTEXT_SHORTCUTS[k1]}.${k2}${l2text.includes("entities") ? "" : `.${l3}`}`))).join("\n")).join("\n")}</ul>\
 ${l2text.includes("entities")
 ? `\n
-You can filter out the ${code(l3)} entities by using a filter function on the entities array:
+You can filter out the entities of ${code(l3)} type by using a filter function on the entities array:
 
 ${pre(
-`${l1.map((k1) => l2.map((k2) => `const entities = ctx.${CONTEXT_SHORTCUTS[k1]}.${k2};`).join("\n")).join("\n")}
+`${l1.map((k1, i) => l2.map((k2, j) => `const entities = ctx.${CONTEXT_SHORTCUTS[k1]}.${k2};${
+  (l1.length > 1 || l2.length > 1) ? (l1.length === i + 1) && (l2.length === j + 1) ? "" : ` // or` : ""
+}`).join("\n")).join("\n")}
 const ${camelCase(l3, "_")}Entities = entities.filter((e) => e.type === "${l3}")`, "ts"
 )}`
 : ""}`
+
   } else {
     throw new Error("Check this out, there is an issue with " + filterQuery);
   }
@@ -77,6 +85,15 @@ const ${camelCase(l3, "_")}Entities = entities.filter((e) => e.type === "${l3}")
 
 function s(arr: unknown[]) {
   return arr.filter(Boolean).length > 1 ? "s" : "";
+}
+
+function plural(arr: unknown[], singular: string) {
+  return arr.filter(Boolean).length > 1
+    ? {
+      "it's": "their",
+      "It's": "Their",
+    }[singular]
+    : singular;
 }
 
 function camelCase(str: string, separator: string) {
@@ -94,6 +111,6 @@ function code(str: string) {
   return `<code>${str}</code>`;
 }
 
-function pre(str: string, lang?: string) {
-  return `<pre data-language="${lang}"><code>${str}</code></pre>`;
+function pre(code: string, lang?: string) {
+  return `<pre data-language="${lang}"><code>${code}</code></pre>`;
 }
--- serve.ts
+++ serve.ts
@@ -1,22 +1,26 @@
 import { serve } from "https://deno.land/std@0.178.0/http/server.ts";
 import { generate } from "./docs.ts";
-import { GRAMMY_VERSION } from "./mod.ts";
+import fqList, { GRAMMY_VERSION } from "./mod.ts";
 
+console.log("Generating docs...");
 const filterQueries = generate();
+console.log("Docs have been generated");
+
+function slug(str: string) {
+  return str.replace(":", "-").replace("_", "-");
+}
 
 const list = `<li>${
-  filterQueries.map((fq) => `
-  <a href="#${
-    fq.filterQuery.replace(":", "-")
-  }" style="text-decoration: none; color: black">#</a></span>
-  <h3 style="display: inline-block" class="acc" id="${
-    fq.filterQuery.replace(":", "-")
-  }">${fq.filterQuery}</h3>
-  <div class="info">${fq.info}</div>
+  filterQueries.map(({ filterQuery: fq, info }) => `
+  <a href="#${slug(fq)}" class="anchor">#</a></span>
+  <h3 style="display: inline-block" class="acc" id="${slug(fq)}">${fq}</h3>
+  <div class="info">${info}</div>
+  <hr>
 `).join("</li>\n<li>")
 }</li>`;
 
 serve(() => {
+  const queries = fqList.sort(() => 0.5 - Math.random()).slice(0, 30);
   const content = `
 <html>
 <head>
@@ -28,15 +32,20 @@ serve(() => {
 }
 
 pre {
-  background: dimgrey;
-  color: white;
+  background: lightgrey;
   padding: 10px;
+  border-radius: 5px;
 }
 
 code {
-  background: dimgrey;
-  color: white;
+  background: lightgrey;
   padding: 2px;
+  border-radius: 2px;
+}
+
+.anchor {
+  color: grey;
+  text-decoration: underline;
 }
 
 li {
@@ -61,34 +70,63 @@ li {
 .info {
   display: none;
   overflow: hidden;
+  padding-bottom: 15px;
 }
 
-.y {
+#title {
   color: #009dca;
+  font-family: monospace;
+  width: 100%;
 }
+
+h2, h3 {
+  font-family: monospace;
+}
+
+p a {
+  color: #009dca;
+  text-decoration: none;
+}
+
 </style>
 </head>
 <body style="padding: 10px">
-  <h1 id="title" class="y">Filter Queries (Y@${GRAMMY_VERSION})</h1>
-  <a href="https://github.com/dcdunkan/filter-queries">source</a>
-  <br>
+  <h1><span id="title">filter::queries</span></h1>
+  <h2>grammY version: ${GRAMMY_VERSION}</h2>
   <p>
     Ever wondered where you can find a full list of valid filter queries?
-    This page lists all of them; this is an auto-generated list of filter queries available in grammY.
+    This page lists all of them! This is an auto-generated list of filter queries available in grammY.
     For more information on filter queries, <a href="https://grammy.dev/guide/filter-queries.html">see official documentation on it.</a>
+    You can find the source code of this site <a href="https://github.com/dcdunkan/filter-queries">here</a>. 
   </p>
   <input type="text" autocomplete="off" id="query" onkeyup="search()" placeholder="Search for queries...">
   <ul id="list">${list}</ul>
   <script>
+  
+  const fqList = ${JSON.stringify(queries)};
+  let index = 0, cursor = 0;
+  const title = document.getElementById("title");
+
+  window.onload = function () {
+    setTimeout(() => {
+      title.style.backgroundColor = "skyblue";
+      setTimeout(() => {
+        title.innerText = "";
+        title.style.backgroundColor = "";
+        type();
+      }, 500);
+    }, 3000);
+  }
+
   function search() {
     const query = document.getElementById("query").value.toLowerCase();
-    const items = document.getElementById("list").children;
+    const items = document.getElementsByTagName("h3");
     for (const item of items) {
       const text = item.textContent || item.innerText;
       if (text.toLowerCase().indexOf(query) > -1) {
-        item.style.display = "";
+        item.parentElement.style.display = "";
       } else {
-        item.style.display = "none";
+        item.parentElement.style.display = "none";
       }
     }
   }
@@ -98,8 +136,29 @@ li {
     acc.addEventListener("click", function () {
       this.classList.toggle("active");
       const info = this.nextElementSibling;
-      info.style.display = info.style.display === "block" ? "none" : "block";    })
+      info.style.display = info.style.display === "block" ? "none" : "block";
+    })
   }
+
+  function type() {
+    const text = fqList[index];
+    if (cursor < text.length) {
+      title.innerText += text[cursor++];
+      setTimeout(type, 75);
+    } else if (cursor === text.length) {
+      setTimeout(() => {
+        title.style.backgroundColor = "skyblue";
+        setTimeout(() => {
+          title.innerText = "";
+          index = (index + 1) >= fqList.length ? 0 : index + 1;
+          cursor = 0;
+          title.style.backgroundColor = "";
+          type();
+        }, 500);
+      }, 2000);
+    }
+  }
+
   </script>
 </body>
 </html>`;
